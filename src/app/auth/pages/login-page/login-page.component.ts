5d012c49d503d1d9b2bd5849c3bd54063a72b0d3
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-19 16:06:32 +0100

    feat(auth): add login and error handling for the user

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,22 +1,31 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
-
-  private fb = inject( FormBuilder);
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-  })
+    email: ['fernando@google.com', [Validators.required, Validators.email]],
+    password: ['1234567', [Validators.required, Validators.minLength(6)]],
+  });
 
-  login(){
-    console.log(this.myForm.value);
+  login() {
+    const { email, password } = this.myForm.value;
+
+    this.authService.login(email, password)
+    .subscribe({
+      next: () => console.log('Todo bien'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error' )
+      }
+    })
   }
-
 }
--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,19 +1,43 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from 'src/environments/environments';
-import { User } from '../interfaces';
+import { LoginResponse, User } from '../interfaces';
 import { AuthStatus } from '../interfaces/auth-status.enum';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private readonly baseUrl: string = environment.baseUrl;
   private http = inject(HttpClient);
 
-  private _currentUser = signal<User|null>(null);
-  private _authStatus = signal<AuthStatus>( AuthStatus.checking );
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
-  constructor() { }
+  public currentUser = computed(() => this._currentUser()); //Señal computada para enseñar el valor del usuario - no modificable
+  public authStatus = computed(() => this._authStatus()); //Señal computada para enseñar el valor del estatus - no modificable
+
+  constructor() {}
+
+  login(email: string, password: string): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/login`; // url al que voy a llegar
+    const body = { email, password }; // petición de la petición http que voy a mandar al backend
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+        console.log({ user, token });
+      }),
+
+      map(() => true),
+
+      catchError((err) => {
+        console.log(err);
+        return throwError(() => 'Algo no sucedió como lo esperaba');
+      })
+    );
+  }
 }
