b56a5b407a4b39f6bf2a2a48e7bb23f55c0d6fc0
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-19 13:30:16 +0100

    build(app): build basic structure of project

---
+++ src/app/dashboard/layouts/dashboard-layouts/dashboard-layouts.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DashboardLayoutsComponent } from './dashboard-layouts.component';
+
+describe('DashboardLayoutsComponent', () => {
+  let component: DashboardLayoutsComponent;
+  let fixture: ComponentFixture<DashboardLayoutsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DashboardLayoutsComponent]
+    });
+    fixture = TestBed.createComponent(DashboardLayoutsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
